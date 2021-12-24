9a97baa70a1e964bf883d588b18bce1aec3a3c66
Micha <61242418+modestmicha@users.noreply.github.com>
2021-12-24 22:37:19 +0000

    Add songs

--- romanize.py
+++ romanize.py
@@ -1,8 +1,11 @@
 import sys
 import json
+import re
 from pythainlp.tokenize import clause_tokenize, word_tokenize
 from pythainlp.transliterate import romanize
 
+re_is_thai = re.compile('[\u0E00-\u0E7F]')
+
 for line in sys.stdin:
-    print(json.dumps(list([romanize(word, engine='thai2rom') for word in word_tokenize(line[:-1])])))
+    print(json.dumps(list([romanize(word, engine='thai2rom') if re_is_thai.match(word) else word for word in word_tokenize(line[:-1])])))
     sys.stdout.flush()
